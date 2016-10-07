04117487d2617113bb5e3f9f904d61461396d0b0
woohajin <woohajin@naver.com>
2016-10-07 20:17:33 +0900

    Create Shakespeare.java

---
+++ Shakespeare.java
@@ -0,0 +1,31 @@
+
+public final class Shakespeare {
+
+    public static final String[] TITLES =
+            {
+                    "cat",
+                    "dog",
+                    "bear",
+                    "panda",
+                    "rabbit",
+                    "elephant",
+                    "tiger",
+                    "bird",
+                    "lion"
+                               };
+
+    public static final int[] IMG =
+            {
+                    R.drawable.cat,
+                    R.drawable.dog,
+                    R.drawable.bear,
+                    R.drawable.panda,
+                    R.drawable.rabbit,
+                    R.drawable.elephant,
+                    R.drawable.tiger,
+                    R.drawable.bird,
+                    R.drawable.lion,
+                    R.drawable.monky
+
+            };
+}
